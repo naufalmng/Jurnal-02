a84c4cba6c047b1784c76432b22fa1309d1d8ed4
naufalmng <35796936+naufalmng@users.noreply.github.com>
2020-09-21 20:01:30 +0700

    HelloWorld.java

---
+++ Praktikum 1/HelloWorld2.java
@@ -0,0 +1,8 @@
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Naufal!");
+		System.out.println("NIM: 6706202146");
+		System.out.println("Kelas: D3IF-44-02");
+	}
+}
